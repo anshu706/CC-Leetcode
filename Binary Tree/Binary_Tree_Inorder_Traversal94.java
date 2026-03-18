148b00f99598bb1c439db8f0fe1fea82d2fee15d
ansh <anshupadhayay>
2026-03-18 20:46:03 +0530

    94

---
+++ Binary Tree/Binary_Tree_Inorder_Traversal94.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+}
