ac1a1e1ab5e06264bf6bfefd52ca401d42749414
ansh <anshupadhayay>
2026-03-18 19:13:50 +0530

    342

---
+++ Recursion/Power_of_Four342.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+
+    if(n==0) return false;
+
+    if(n==1) return true;
+
+    if(n%4 == 1) return false;
+
+    if(n%4 == 2 ) return false;
+
+    if(n%4 == 3 ) return false;
+
+    return isPowerOfFour(n/4);
+
+    }
+}
