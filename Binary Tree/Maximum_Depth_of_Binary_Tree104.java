3d02dfa6d4f9b9ae15eb302a4ed5765b256466bc
ansh <anshupadhayay>
2026-03-18 20:53:23 +0530

    104

---
+++ Binary Tree/Maximum_Depth_of_Binary_Tree104.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        // base case
+        if(root == null){
+            return 0;
+        }
+
+        //recursive case
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
