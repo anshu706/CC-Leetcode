f3d591162420ab1845f65a5ea36971096ddee5b0
ansh <anshupadhayay>
2026-03-18 19:10:36 +0530

    326

---
+++ Recursion/Power_of_Three326.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+    if(n==0) return false;
+
+    if(n==1) return true; 
+    
+    if(n % 3 == 1 || n % 3 == 2) return false;
+    
+    return isPowerOfThree(n/3);
+    
+    }
+}
