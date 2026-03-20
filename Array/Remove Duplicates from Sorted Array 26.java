7d323186b4d00f43dbc5439dc31efde62a388422
ansh <anshupadhayay>
2026-03-20 10:05:03 +0530

    26

--- Array/Remove Duplicates from Sorted Array 26.java
+++ Array/Remove Duplicates from Sorted Array 26.java
@@ -1,5 +1,4 @@
-
-class Solution { 
+class Solution {
 
     public int removeDuplicates(int[] nums) {
         int k = 1;
--- README.md
+++
@@ -1,1 +0,0 @@
-Here are the Questions solved on Leetcode for Competitive Coding.
