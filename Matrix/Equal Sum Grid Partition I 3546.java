1aaf95476bd6d57fb5dee15d005261b12ab89e70
ansh <anshupadhayay>
2026-03-25 09:35:59 +0530

    3546

---
+++ Matrix/Equal Sum Grid Partition I 3546.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        long total = 0;
+        for (int[] row : grid)
+            for (int val : row)
+                total += val;
+
+        // Check horizontal cuts (between rows)
+        long prefix = 0;
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int val : grid[i])
+                prefix += val;
+            if (prefix * 2 == total)
+                return true;
+        }
+
+        // Check vertical cuts (between columns)
+        int m = grid.length, n = grid[0].length;
+        prefix = 0;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++)
+                prefix += grid[i][j];
+            if (prefix * 2 == total)
+                return true;
+        }
+
+        return false;
+    }
+}
