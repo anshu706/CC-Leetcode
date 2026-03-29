ef9f73c454987874637c936e9988199ae96b4a0f
ansh <anshupadhayay>
2026-03-29 17:37:11 +0530

    128

---
+++ Array/Longest Consecutive Sequence 128.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int maxLen = 0;
+
+        for (int num : set) {
+            // Only start counting from the beginning of a sequence
+            if (!set.contains(num - 1)) {
+                int length = 1;
+                while (set.contains(num + length)) length++;
+                maxLen = Math.max(maxLen, length);
+            }
+        }
+
+        return maxLen;
+    }
+}
