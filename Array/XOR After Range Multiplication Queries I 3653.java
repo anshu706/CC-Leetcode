866b843a10a2b707e823b346d2ed7da4282d70e4
ansh <anshupadhayay>
2026-04-08 18:16:32 +0530

    3653

---
+++ Array/XOR After Range Multiplication Queries I 3653.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        final int MOD = 1_000_000_007;
+
+        // Process each query
+        for (int[] query : queries) {
+            int l = query[0];
+            int r = query[1];
+            int k = query[2];
+            int v = query[3];
+
+            for (int idx = l; idx <= r; idx += k) {
+                nums[idx] = (int)((1L * nums[idx] * v) % MOD);
+            }
+        }
+
+        // XOR all elements
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
