9568a0eb314b96a9fb51f07df9a1f0420dca7d8e
ansh <anshupadhayay>
2026-04-22 11:17:11 +0530

    2452

---
+++ String/Words Within Two Edits of Dictionary 2452.java
@@ -0,0 +1,24 @@
+
+class Solution {
+
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+
+        for (String query : queries) {
+            for (String dictWord : dictionary) {
+                int diff = 0;
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != dictWord.charAt(i)) {
+                        diff++;
+                    }
+                }
+                if (diff <= 2) {
+                    result.add(query);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
